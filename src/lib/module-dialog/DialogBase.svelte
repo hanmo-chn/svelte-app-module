46634e098ea9e0ad1a019a257be9f8bf58719865
henryfeng <huili.f@gmail.com>
2023-03-17 10:26:04 +0800

    batch commit a group files

--- src/lib/module-dialog/DialogBase.svelte
+++ src/lib/module-dialog/DialogBase.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
+    import {createEventDispatcher} from "svelte";
+
     export let width: string = "600px";
     export let height: string = "400px";
     export let title: string = '';
 
+    const dispatch = createEventDispatcher();
+
+    export const close = () => {
+        dispatch('close');
+    }
+
 </script>
 <div class="tsui-module-dialog" style="width: {width}; height: {height}">
     <div class="dialog-title-bar">
-        <span>{title}</span>
+        <div style="flex: 1 1 auto; white-space: nowrap; overflow: hidden; text-overflow: ellipsis">
+            <span>{title}</span>
+        </div>
+        {#if $$slots['title-bar-icons']}
+            <div class="title-bar-icons">
+                <slot name="title-bar-icons"/>
+            </div>
+        {/if}
     </div>
+    {#if $$slots['header']}
+        <div class="dialog-header">
+            <slot name="header"/>
+        </div>
+    {/if}
     <div class="dialog-content">
         <slot/>
     </div>
